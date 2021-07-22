a78c1e6e13a8a21aa73f5e7158d95f4ccb355608
Mrinal Mayank <65720872+mrinalmayank7@users.noreply.github.com>
2021-07-22 15:59:51 +0530

    Update settings.py

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -22,12 +22,12 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,"templates")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nd!w_+xzz@hezk6@##h_-_e^t(&(#mq0s&g+m9nze81r-n%)#i'
+SECRET_KEY = '***************************************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '127.0.0.1' ,'localhost']
+ALLOWED_HOSTS = [ ]
 
 
 # Application definition
@@ -139,5 +139,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST ='smtp.gmail.com'
 EMAIL_PORT ='587'
 EMAIL_USE_TLS =True
-EMAIL_HOST_USER ='nosh.beta.version@gmail.com'
-EMAIL_HOST_PASSWORD ='ngflkjqcdxppsxry'
+EMAIL_HOST_USER ='****************************************'
+EMAIL_HOST_PASSWORD ='************************************************'
